e5ad82507894e24da2f740ea4984dea047a23ab0
yonghun16 <yonghun16@naver.com>
2025-07-29 21:59:58 +0900

    Update page.tsx

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -1,22 +1,25 @@
-import { type Metadata } from "next"; 
+import { type Metadata } from "next";
 import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-  params: { id: string };
-}
-
+// generateMetadata 함수
 export async function generateMetadata(
-  { params: { id } }: IParams,
+  { params }: { params: { id: string } }
 ): Promise<Metadata> {
-  const movie = await getMovie(id);
+  const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+// page 컴포넌트
+export default async function MovieDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
